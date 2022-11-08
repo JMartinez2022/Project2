5918c550a100239d42cd0fc59b057f2b7447984d
Jaime Martinez <Jaime Martinez@DESKTOP-M0R1FSI.mshome.net>
2022-11-08 11:34:31 +0100

    CAMBIO4

--- p1/src/git01/TestGit.java
+++ p1/src/git01/TestGit.java
@@ -1,8 +1,10 @@
 package git01;
+importejava.util.Scanner;
 
 public class TestGit {
 	public static void main(String[]argos) {
-		String name="Bugs Bunny";
-		System.out.println("Hello "+name+"!");
+		Scannerscanner=newScanner(System.in); String
+		name=scanner.nextLine(); System.out.println("Hello 
+		"+name+"!"); scanner.close();
 		}
 }
